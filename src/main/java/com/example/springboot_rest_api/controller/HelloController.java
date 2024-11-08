fc2912a9261b48e362df627a1467863243405a8d
devopswithamir <152407986+rahat3783@users.noreply.github.com>
2024-11-08 08:59:06 +0530

    Update HelloController.java

--- src/main/java/com/example/springboot_rest_api/controller/HelloController.java
+++ src/main/java/com/example/springboot_rest_api/controller/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello, Welcome to My New Home Mannat!";
+        return "Hello, This is CICD Jenkins Project!";
     }
 }
